2deb5696cd567e360ae549596a003530f9f805e5
chris-lawbrokr <chris@lawbrokr.com>
2026-01-09 11:51:56 -0500

    Initial buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@/stories/Button";
-
+'use client'
+import { Button } from "@/stories/Button/Button";
 export default function Home() {
   return (
-    <div>
-      <main>
-        <Button
-          label="Button"
-          onClick={() => {}}
-          primary
-        />
-      </main>
-    </div>
+    <main className="h-screen w-screen flex items-center justify-center">
+      <Button
+        label="Button"
+        onClick={() => {}}
+        primary
+      />
+    </main>
   );
 }

c0dbe549c5fc5bcd8df0000bc95165620ea1f188
chris-lawbrokr <chris@lawbrokr.com>
2026-01-09 13:26:46 -0500

    Update Header.tsx

--- stories/Header/Header.tsx
+++ stories/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from './Button';
+import { Button } from '../Button/Button';
 import './header.css';
 
 type User = {
@@ -45,7 +45,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button variant="primary" size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>
